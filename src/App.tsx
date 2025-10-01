f200d7031b8557418f0366d737545417c3b434a0
RamaRayyan <rama.rayyan@nusabs.sch.id>
2025-10-01 12:02:38 +0700

    add image in about me and setting hero

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ import patenT from "./assets/paten T.png";
 import patenR from "./assets/paten R.png";
 import patenL from "./assets/paten L.png";
 import hero from "./assets/hero.png";
+import tentangkami from "./assets/fotobareng.png"
 import PRR from "./assets/Pipa_Radial.png"
 import { Image } from "lucide-react";
 
@@ -255,23 +256,37 @@ function App() {
 
 
       {/* Tentang Kami */}
-      <section id="tentang-kami" className="py-20 bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center mb-16">
-            <h2 className="text-4xl font-bold text-gray-900 mb-4">
-              Tentang Kami
-            </h2>
-            <p className="text-xl text-gray-600 max-w-3xl mx-auto">
-              CV. Rekayasa Nusa Mandiri adalah perusahaan yang bergerak di
-              bidang inovasi teknologi lingkungan dan infrastruktur. Kami
-              berkomitmen menghadirkan solusi cerdas untuk konservasi air tanah,
-              pengendalian banjir, dan keberlanjutan lingkungan.
-            </p>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-16">
-          </div>
-        </div>
-      </section>
+     <section id="tentang-kami" className="pt-20 pb-10 bg-white">
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="text-center mb-12">
+      <h2 className="text-4xl font-bold text-gray-900 mb-4">
+        Tentang Kami
+      </h2>
+      <p className="text-xl text-gray-600 max-w-3xl mx-auto">
+        CV. Rekayasa Nusa Mandiri adalah perusahaan yang bergerak di
+        bidang inovasi teknologi lingkungan dan infrastruktur. Kami
+        berkomitmen menghadirkan solusi cerdas untuk konservasi air tanah,
+        pengendalian banjir, dan keberlanjutan lingkungan.
+      </p>
+    </div>
+
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-6">
+      {/* isi tambahan kalau ada */}
+    </div>
+
+    {/* Foto lebih rapet atas-bawah */}
+    <div className="w-full flex justify-center">
+      <img
+        src={tentangkami}
+        alt="tentangKami"
+        className="max-w-3xl w-full h-auto object-contain rounded-2xl shadow-md"
+      />
+    </div>
+  </div>
+</section>
+
+
+
 
       {/* Produk Unggulan */}
       <section id="produk" className="py-20 bg-gray-50">
