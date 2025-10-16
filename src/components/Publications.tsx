ce3b852d0320c8c2b41cbd73d1452b935bb54130
RamaRayyan <rama.rayyan@nusabs.sch.id>
2025-10-16 14:36:58 +0700

    add field in publications

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -5,13 +5,105 @@ import { ArrowLeft } from "lucide-react";
 
 export const publicationsData = [
   {
+    year: 2024,
+    items: [
+      {
+        category: "Seminar",
+        title: "Upaya Mengatasi Permasalahan Kekurangan Air dengan Pipa Resapan Horisontal di Kelurahan Jabungan Kota Semarang",
+        authors: "-",
+        link: "https://new-conference.unisma.ac.id/index.php/KOPEMAS/article/view/608 ",
+      },
+      {
+        category: "Jurnal",
+        title: "Analisa Kebutuhan Pipa Resapan Horizontal Akibat Perubahan Tataguna Lahan Pada Daerah Aliran Sungai Lutung",
+        authors: "Alfira Setyowati, Cindy Salsabila, Edy Susilo, Diah Setyati Budiningrum.",
+        link: "https://journal.unisnu.ac.id/CES/article/download/874/423/  ",
+      },
+    ],
+  },
+  {
+    year: 2023,
+    items: [
+      {
+        category: "Jurnal",
+        title: "Reduction of Flood Discharge Using Perforated Horizontal Recharge Pipes",
+        authors: "Edy Susilo1, Diah Setyati Budiningrum, Anik Kustirini.",
+        link: "https://jurnal.polines.ac.id/index.php/wahana/article/view/4794/108798  ",
+      },
+      {
+        category: "Jurnal",
+        title: "Realizing Zero Delta Q Due to the Change of Land-Use Using Horizontal Recharge Pipe",
+        authors: "Edy Susilo, Diah Setyati Budiningrum, Anik Kustirini.",
+        link: "https://iopscience.iop.org/article/10.1088/1755-1315/1177/1/012008/meta ",
+      },
+    ],
+  },
+  {
+    year: 2022,
+    items: [
+      {
+        category: "Seminar",
+        title: "Realizing Zero Delta Q Due to the Change of Land-Use Using Horizontal Recharge Pipe",
+        authors: "-",
+        link: "",
+      },
+      {
+        category: "Seminar",
+        title: "Seminar Civi Engineering Environmental and Disaster Risk Management Symposium ",
+        authors: "-",
+        link: "",
+      },
+      {
+        category: "Jurnal",
+        title: "Studi Penanganan Genangan Banjir Dan Rob Sungai Meduri-Bremi Kota Pekalongan",
+        authors: "Edy Susilo, Hani Purwanti, Satria Pinandita.",
+        link: "https://jurnal.pekalongankota.go.id/index.php/litbang/article/view/224 ",
+      },
+      {
+        category: "Jurnal",
+        title: "Evaluasi Fisik Pemasangan Pipa Resapan Horizontal (PRH) di Kecamatan Gajahmungkur Kota Semarang",
+        authors: "Fitria Maya Lestari, Mudjiastuti Handajani, Edy Susilo, Ahmad Hakim Bintang Kuncoro, Risdiana Ch. Afifah.",
+        link: "https://riptek.semarangkota.go.id/index.php/riptek/article/view/229 ",
+      },
+    ],
+  },
+  {
+    year: 2021,
+    items: [
+      {
+        category: "Seminar",
+        title: "Bebas Banjir Berbasis Zero Delta Q",
+        authors: "-",
+        link: "",
+      },
+    ],
+  },
+  {
     year: 2020,
     items: [
       {
-        venue: "FDGBI",
+        category: "Seminar",
         title: "Analisa Kebutuhan Pipa Resapan Horisontal Akibat Perubahan Tataguna Lahan",
         authors: "-",
-        link: "-",
+        link: "",
+      },
+      {
+        category: "Seminar",
+        title: "Analisa Kebutuhan Pipa Resapan Horisontal Akibat Perubahan Tataguna Lahan",
+        authors: "-",
+        link: "",
+      },
+      {
+        category: "Jurnal",
+        title: "Perforated Horizontal Recharge Pipe Modeling with Non-Linear Regression",
+        authors: "Edy Susilo, Suripin, Suharyanto.",
+        link: "http://www.irphouse.com/ijert20/ijertv13n7_26.pdf",
+      },
+      {
+        category: "Jurnal",
+        title: "Analisa Kebutuhan Pipa Resapan Horisontal dalam Mengimplementasikan Zero Delta Q Policy",
+        authors: "Ratna Chuswatun Kholifah, Rindang Aldhyantie, Edy Susilo, Diah Setyati Budiningrum.",
+        link: "https://riptek.semarangkota.go.id/index.php/riptek/article/download/135/106 ",
       },
     ],
   },
@@ -19,7 +111,7 @@ export const publicationsData = [
     year: 2019,
     items: [
       {
-        venue: "TSUS",
+        category: "Jurnal",
         title: "Kinerja Pipa Resapan Sebagai Pendukung Konservasi Air",
         authors: "Edy Susilo, Diah Setyati Setyati Budiningrum.",
         link: "https://journals.usm.ac.id/index.php/teknika/article/view/1806 ",
@@ -30,9 +122,9 @@ export const publicationsData = [
     year: 2018,
     items: [
       {
-        venue: "EDP",
+        category: "Jurnal",
         title: "Field performance of shallow recharge well",
-        authors: "EDP Sciences",
+        authors: "EDP Sciences.",
         link: "https://doi.org/10.1051/matecconf/201819505006",
       },
     ],
@@ -40,7 +132,7 @@ export const publicationsData = [
 ];
 
 type Publication = {
-  venue: string;
+  category: string;
   title: string;
   authors: string;
   link: string;
@@ -113,7 +205,7 @@ export const Publications: React.FC<PublicationsProps> = ({ data }) => {
                         className="flex gap-4 items-start bg-gray-100 p-5 rounded-xl hover:bg-blue-50 transition-colors shadow-sm"
                       >
                         <span className="bg-blue-600 text-white px-3 py-1 rounded-md text-sm font-semibold">
-                          {item.venue}
+                          {item.category}
                         </span>
 
                         <div className="flex-1">
